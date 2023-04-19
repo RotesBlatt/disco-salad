bf5a2ad82b48fbfab8bce06782869dc4978c3c17
RotesBlatt <RotesBlatt@users.noreply.github.com>
2023-04-19 20:14:44 +0200

    Added loading and checking for guilds config file and if the user is using the correct text-channel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import fs from "node:fs"; 
+import path from "node:path";
 import * as dotenv from "dotenv";
 import loadAllCommands from "./utils/retrieve-commands";
-import { ClientAdaptation, CustomGuild } from "./types/bot-types";
+import { ClientAdaptation, CustomGuild, SettingsOptions } from "./types/bot-types";
 import { Client, Collection, Events, GatewayIntentBits, ActivityType } from "discord.js";
 
 dotenv.config();
@@ -14,16 +16,23 @@ const clientAdapter: ClientAdaptation = {
     client: botClient,
     commands: new Collection(),
     guildCollection: new Collection(),
-} 
+};
 
 clientAdapter.client.once(Events.ClientReady, async c => {
     console.log(`[INFO] Logged in as ${c.user.tag}`);
     clientAdapter.commands = await loadAllCommands();
     clientAdapter.client.user?.setActivity({name: 'the music', type: ActivityType.Playing});
-})
+});
 
 clientAdapter.client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
+    if(!interaction.isChatInputCommand()) { return; };
+
+    const guildConfig: SettingsOptions = (await import(`../guild-data/${interaction.guildId}.json`, {assert: {type: "json"}})).default;
+
+    if(guildConfig.textChannelId && guildConfig.textChannelId !== interaction.channelId){ 
+        await interaction.reply({content: `You need to use the channel <#${guildConfig.textChannelId}> to use the bot`, ephemeral: true});
+        return; 
+    };
 
     if(!clientAdapter.guildCollection.has(interaction.guildId!)){
         clientAdapter.guildCollection.set(interaction.guildId!, new CustomGuild());
@@ -36,11 +45,31 @@ clientAdapter.client.on(Events.InteractionCreate, async interaction => {
     }
 
     try {
-        await command.execute(interaction, clientAdapter);
+        await command.execute(interaction, clientAdapter, guildConfig);
     } catch (error) {
         console.error(error);
         await interaction.reply({content: 'There was en error while executing this command!', ephemeral: true});
     }
-})
+});
+
+clientAdapter.client.on(Events.GuildCreate, guild => {
+    const configFilePath = path.resolve(`guild-data/${guild.id}.json`);
+
+    if(fs.existsSync(configFilePath)){
+        console.log(`[INFO] Found config file for guild: ${guild.name}`);
+        return;
+    }
+
+    const emptySettings: SettingsOptions = {};
+    const settingsOutputToFile = JSON.stringify(emptySettings);
+
+    fs.writeFile(configFilePath, settingsOutputToFile, 'utf-8', (err) => {
+        if(err){
+            console.log(`[ERROR] There was an error writing the settings file for guild ${guild.name}`);
+            console.log(err);
+        }
+        console.log(`[INFO] Created a new settings file for guild: ${guild.name}`);
+    })
+});
 
 clientAdapter.client.login(process.env.BOT_TOKEN!);
